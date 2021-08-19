9732e1916952a820fc399eb6ecc939f3c89e53f3
The Yule <xander@yoo-babobo.com>
2021-08-19 18:18:21 -0400

    1.1.0
    Added commands to the Cool Bot.

--- index.js
+++ index.js
@@ -7,7 +7,28 @@ bot.once("ready", () => {
 });
 
 bot.on("messageCreate", message => {
-    console.log(message.content);
+    if (message.author.bot || message.webhookId || !message.content.startsWith(config.prefix)) return;
+
+    const raw_args = message.content.slice(config.prefix.length).trim().split(/ +/);
+    const command = raw_args.shift().toLowerCase();
+    const args = raw_args.slice(0);
+
+    switch (command) {
+        case "ping":
+            message.channel.send("Pong! :ping_pong:");
+            break;
+        case "say":
+            if (!args.length) return message.reply("You need to provide a message!");
+
+            message.channel.send(args.join(" "));
+            break;
+        case "kill":
+            var user = message.mentions.members.first();
+            if (!user) return message.reply("Please mention a user or bot!");
+
+            message.channel.send(user.toString() + " died!");
+            break;
+    }
 });
 
 bot.login(config.token);
